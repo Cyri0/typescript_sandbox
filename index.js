6e0489b12ee271a725c9cf23de9ccc0680ca5520
Cyri0 <nemestamas94@gmail.com>
2024-09-04 10:06:09 +0200

    number guess

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 "use strict";
-let greet = "Hello TypesScript!";
-console.log(greet);
+const numEl = document.getElementById("inputNum");
+const resultEl = document.getElementById("result");
+const theNumber = Math.floor(Math.random() * 100) + 1;
+function guess() {
+    const guessNumber = numEl.valueAsNumber;
+    if (isCorrect(guessNumber)) {
+        resultEl.innerText = "Eltaláltad!";
+    }
+    else {
+        if (guessNumber < theNumber) {
+            resultEl.innerText = "A gondolt szám nagyobb!";
+        }
+        else {
+            resultEl.innerText = "A gondolt szám kisebb!";
+        }
+    }
+}
+function isCorrect(guessNumber) {
+    return guessNumber === theNumber;
+}
--- index.ts
+++ index.ts
@@ -1,2 +1,22 @@
-let greet: string = "Hello TypesScript!"
-console.log(greet)
+const numEl: HTMLInputElement = document.getElementById("inputNum") as HTMLInputElement
+
+const resultEl: HTMLParagraphElement = document.getElementById("result") as HTMLParagraphElement
+
+const theNumber: number = Math.floor(Math.random() * 100) + 1
+
+function guess(){
+    const guessNumber: number = numEl.valueAsNumber
+    if(isCorrect(guessNumber)){
+        resultEl.innerText = "Eltaláltad!"
+    }else{
+        if(guessNumber < theNumber){
+            resultEl.innerText = "A gondolt szám nagyobb!"
+        }else{
+            resultEl.innerText = "A gondolt szám kisebb!"
+        }
+    }
+}
+
+function isCorrect(guessNumber: number) : boolean {
+    return guessNumber === theNumber
+}
